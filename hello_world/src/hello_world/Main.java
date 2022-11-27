52b81a67690895b47c6b42a6c3bd85e92f357db6
Denerval s Pelizario <denervalspelizario@gmail.com>
2022-11-27 16:04:13 -0300

    estudo java  concatenacao, input  e tipos  de dados

--- hello_world/src/hello_world/Main.java
+++ hello_world/src/hello_world/Main.java
@@ -1,10 +1,40 @@
 package hello_world;
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		System.out.print("o print escreve dado sem quebra de linha -- ");
 		System.out.println("Hello World!!");
-
+		System.out.println("já o println escreve o dado com quebra de linha");
+		System.out.println("Hello World!!");
+		System.out.println("\nvariaveis em java\n");
+		
+		int y = 32;
+		int x = 35;
+		System.out.println(y + x);
+		
+		double a = 10.35784;
+		
+		System.out.printf("%.2f\n",a);
+		System.out.printf("%.3f\n",a);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.3f\n",a);
+		System.out.println("\nConcatenar no Java\n");
+		
+		System.out.println("Resultado = "+ x + " Metros");
+		
+		System.out.println("\nConcatenar varias variáveis\n");
+		
+		String nome = "Denerval S pelizario";
+		int idade = 35;
+		double salario = 38.000;
+		
+		System.out.printf("%s tem %d anos e ganha R$ %.3f mil reais /n", nome, idade, salario);
+		
+		
+				
 	}
 
 }
