32857d44e4259b852315b4ebea5b8340c634ad40
Denerval s Pelizario <denervalspelizario@gmail.com>
2022-12-03 12:57:52 -0300

    update estudo java estrutura repetitiva while

---
+++ EstruturaRepetitivaJava/src/Main.java
@@ -0,0 +1,33 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	
+	
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		int valorSoma = 0;
+		
+		while(x != 0) {
+			
+			valorSoma += x;
+			x = sc.nextInt();
+			
+		}
+		
+		System.out.println("O valor da soma dos numeros digitados é " + valorSoma);
+		
+		
+		sc.close();
+	
+		
+		
+	}
+
+}
