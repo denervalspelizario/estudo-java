bfaf1d4d0ea687ac0b5a507a2db673200cf1e5fb
denervalspelizario <denervalspelizario@gmail.com>
2022-12-11 06:31:04 -0300

    estudo java exercicio produto POO

---
+++ estoquePoo/src/aplication/Program.java
@@ -0,0 +1,10 @@
+package aplication;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// 1 se cria  a aplicacao  2- cria a classe separado onde se coloca as atribuicoes e metodos
+
+	}
+
+}
---
+++ estoquePoo/src/entities/Product.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Product {
+	
+	public String name;   // 3 - criando as atibuicoes e metodos
+	public double price;
+	public int quantity;
+	
+	
+	
+	public double totalValueInStock;
+	
+}
