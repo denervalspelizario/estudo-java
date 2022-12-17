b637ef37b06225adc7a5909ce43b042613ff994a
denervalspelizario <denervalspelizario@gmail.com>
2022-12-17 06:32:22 -0300

    estudo java inicio de resolucao exercicio 3

--- Notas/src/entities/notasPoo.java
+++ Notas/src/entities/notasPoo.java
@@ -1,5 +1,29 @@
 package entities;
 
 public class notasPoo {
-
+	public String nameAluno;   //  criando as atibuicoes e metodos(ou funcoes)  
+	public double nota1;
+	public double nota2;
+	public double nota3;
+	
+	
+	public String aprovacaoAluno() {  //  aviso para manha usar esse if else  no class principal
+		
+		 double notaBase = 60.00;	
+		 double notaFinal = (nota1 + nota2 + nota3) / 3;
+		 
+		 	if(notaFinal >= notaBase ) {
+		 		
+					System.out.println("FINAL GRADE ="+ notaFinal );
+					System.out.println("Pass");
+				} else {
+					
+					System.out.println("FINAL GRADE ="+ notaFinal );
+					System.out.println("Failed");
+					
+					double notaMissing = notaBase - notaFinal;
+					System.out.println("MISSING "+ notaMissing +" Points");
+				};
+			
+	}
 }
--- Notas/src/programNotas/program.java
+++ Notas/src/programNotas/program.java
@@ -1,13 +1,33 @@
 package programNotas;
 
 import entities.notasPoo;
+import java.util.Locale;
+import java.util.Scanner;
+
+
 
 
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner inputDado = new Scanner(System.in);
+		
+		notasPoo notasInput = new notasPoo(); 
+		
+		System.out.println("Digite o nome do Aluno: ");
+		notasInput.nameAluno = inputDado.nextLine();
+		
+		System.out.println("Digite as notas: ");
+		notasInput.nota1 = inputDado.nextDouble();
+		notasInput.nota2 = inputDado.nextDouble();
+		notasInput.nota3 = inputDado.nextDouble();
 
+		String aprovacaoAluno = notasInput.aprovacaoAluno();
+
+		
+		
 	}
 
 }
