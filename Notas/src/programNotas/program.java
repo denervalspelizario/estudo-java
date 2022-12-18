fd7a966e458a352afe28b73fcd1e724d03a38a17
denervalspelizario <denervalspelizario@gmail.com>
2022-12-18 06:47:38 -0300

    estudo java resolucao exercicio 3 poo

--- Notas/src/programNotas/program.java
+++ Notas/src/programNotas/program.java
@@ -21,10 +21,40 @@ public class program {
 		
 		System.out.println("Digite as notas: ");
 		notasInput.nota1 = inputDado.nextDouble();
-		notasInput.nota2 = inputDado.nextDouble();
-		notasInput.nota3 = inputDado.nextDouble();
+		while(notasInput.nota1 > 30.00) {
+			System.out.println("O valor da primeira nota deve ser abaixo de 30");
+			notasInput.nota1 = inputDado.nextDouble();
 
-		String aprovacaoAluno = notasInput.aprovacaoAluno();
+		};
+		notasInput.nota2 = inputDado.nextDouble();
+		while(notasInput.nota2 > 35.00) {
+			System.out.println("O valor da segunda nota deve ser abaixo de 35");
+			notasInput.nota2 = inputDado.nextDouble();
+
+		};
+		notasInput.nota3 = inputDado.nextDouble();
+		while(notasInput.nota3 > 35.00) {
+			System.out.println("O valor da terceira nota deve ser abaixo de 35");
+			notasInput.nota3 = inputDado.nextDouble();
+
+		};
+
+		double  notaFinalAluno = notasInput.notaFinalAluno();
+		double notaBase = 60.00;
+		
+		
+		if(notaFinalAluno >= notaBase ) {
+	 		
+			System.out.printf("FINAL GRADE =  %.2f%n", notaFinalAluno );
+			System.out.println("Pass");
+		} else {
+			
+			System.out.printf("FINAL GRADE =  %.2f%n", notaFinalAluno );
+			System.out.println("Failed");
+			
+			double notaMissing = notaBase - notaFinalAluno;
+			System.out.printf("MISSING  %.2f%n", notaMissing +" Points");
+		};
 
 		
 		
